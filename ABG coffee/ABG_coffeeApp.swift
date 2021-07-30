b5c349652b90973fd01116d8e514d8d6b65a4425
dick <frydonut@gmail.com>
2021-07-30 09:50:03 +0800

    Initial Commit

---
+++ ABG coffee/ABG_coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABG_coffeeApp.swift
+//  ABG coffee
+//
+//  Created by Dick Chen on 2021/7/30.
+//
+
+import SwiftUI
+
+@main
+struct ABG_coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
